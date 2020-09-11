70d849482b2da95781cf72df3457e5cee7b3d3ad
J <jeremy@zunkworks.com>
2020-09-11 15:17:28 -0500

    add ipv4address data

--- Sources/NetworkLinux/IPv4Address.swift
+++ Sources/NetworkLinux/IPv4Address.swift
@@ -10,14 +10,25 @@ import Foundation
 public struct IPv4Address
 {
     public var address: String
+    public var data: Data
     
     public init?(_ address: String)
     {
         self.address=address
+        
+        let parts = address.split(separator: ".")
+        guard let part0 = UInt8(parts[0]) else { return nil}
+        guard let part1 = UInt8(parts[1]) else { return nil}
+        guard let part2 = UInt8(parts[2]) else { return nil}
+        guard let part3 = UInt8(parts[3]) else { return nil}
+        let array = [part0, part1, part2, part3]
+        self.data = Data(array)
+    
     }
     
     public init?(_ rawValue: Data, _ interface: NWInterface? = nil)
     {
         self.address = String(Int(rawValue[0])) + "." + String(Int(rawValue[1])) + "." + String(Int(rawValue[2])) + "." + String(Int(rawValue[3]))
+        self.data = rawValue
     }
 }

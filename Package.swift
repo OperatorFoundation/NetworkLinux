5d9d569685eae57b61d2a1233a49be30ab0dcb08
Dr. Brandon Wiley <github@blanu.net>
2021-12-06 23:18:46 -0600

    Fixed package URLs

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Socket", url: "https://github.com/Kitura/BlueSocket", from: "2.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.2"),
+        .package(name: "Socket", url: "https://github.com/Kitura/BlueSocket.git", from: "2.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", from: "1.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

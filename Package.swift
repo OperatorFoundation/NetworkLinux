ee91bf2ac09f4ef5c8d0f3c06bfc0a382d08ffb7
consuelita <adelita@operatorfoundation.org>
2022-01-20 13:56:49 -0600

    Depend on Operator fork of BlueSocket

--- Package.swift
+++ Package.swift
@@ -1,26 +1,21 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "NetworkLinux",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NetworkLinux",
             targets: ["NetworkLinux"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Socket", url: "https://github.com/Kitura/BlueSocket.git", from: "2.0.2"),
+        .package(name: "Socket", url: "https://github.com/OperatorFoundation/BlueSocket.git", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", from: "1.2.2"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NetworkLinux",
             dependencies: ["Socket", "SwiftHexTools"]),
